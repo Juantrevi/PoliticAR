d00e2df178900d4898ef22bad2a1c7cae6736968
Sergio Romano <sergiojavierromano@gmail.com>
2022-01-24 20:55:53 -0300

    Noticias: Creacion de entidades, servicios, repositorio

---
+++ src/main/java/com/example/proyecto/errores/ErrorServicio.java
@@ -0,0 +1,14 @@
+
+package com.example.proyecto.errores;
+
+/**
+ *
+ * @author Sergio
+ */
+public class ErrorServicio extends Exception{
+    
+   public ErrorServicio (String msg) {
+       super(msg);
+   } 
+    
+}
